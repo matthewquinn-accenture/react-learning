14fdafd1061f95a29f4a878b8e2620a7105d9fbe
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-06-05 07:47:22 -0400

    Created edit and remove function

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -3,13 +3,27 @@ import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
 
 class Note extends Component {
+    constructor(props) {
+        super(props)
+        this.edit = this.edit.bind(this)
+        this.remove = this.remove.bind(this)
+    }
+
+    edit() {
+        alert('editing note')
+    }
+
+    remove() {
+        alert('removing note')
+    }
+
     render() {
         return (
             <div className="note"> 
                 <p>I'm awesome</p>
                 <span>
-                    <button id="edit"> <FaPencil /> </button>
-                    <button id="remove"> <FaTrash /> </button>
+                    <button onClick={this.edit} id="edit"> <FaPencil /> </button>
+                    <button onClick={this.remove} id="remove"> <FaTrash /> </button>
                 </span>
             </div>
         )
