b2306a66c49138ba9e4923f9e496386356eba0a3
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-06-05 19:13:58 -0400

    Created a form display, refactored some

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            editing: false
+        }
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+        this.checkState = this.checkState.bind(this)
     }
 
     edit() {
-        alert('editing note')
+        this.setState({
+            editing: true
+        })
     }
 
     remove() {
         alert('removing note')
     }
 
-    render() {
+    save() {
+        alert('saved!')
+    }
+
+    renderForm() {
+        return (
+            <div className="note">
+                <form>
+                    <textarea />
+                    <button onClick={this.save}><FaFloppyO /></button>
+                </form>
+            </div>
+        )
+    }
+
+    renderDisplay() {
         return (
             <div className="note"> 
                 <p>I'm awesome</p>
@@ -27,6 +52,20 @@ class Note extends Component {
                 </span>
             </div>
         )
+    }
+
+    checkState() {
+        let display = this.renderDisplay() 
+
+        if (this.state.editing) {
+            display = this.renderForm()
+        }
+
+        return display
+    }
+
+    render() {
+        return this.checkState()
     }
 }
 
