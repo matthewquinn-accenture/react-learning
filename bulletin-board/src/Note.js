03a054b8d36732e3383cabc2e3dc88529f554169
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-06-08 20:57:43 -0400

    Scattered the notes all over the board

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -15,6 +15,19 @@ class Note extends Component {
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
         this.checkState = this.checkState.bind(this)
+        this.randomBetween = this.randomBetween.bind(this)
+    }
+
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+            transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+        }
+    }
+
+    randomBetween(x, y, s) {
+        return x + Math.ceil(Math.random() * (y-x)) + s
     }
 
     edit() {
@@ -36,7 +49,7 @@ class Note extends Component {
 
     renderForm() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <form onSubmit={this.save}>
                     <textarea ref={input => this._newText = input} />
                     <button type="button" id="save"><FaFloppyO /></button>
@@ -47,7 +60,7 @@ class Note extends Component {
 
     renderDisplay() {
         return (
-            <div className="note"> 
+            <div className="note" style={this.style}> 
                 <p>{this.props.children}</p>
                 <span>
                     <button type="button" onClick={this.edit} id="edit"> <FaPencil /> </button>
