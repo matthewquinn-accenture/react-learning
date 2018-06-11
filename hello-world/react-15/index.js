41a01cbfd37162b43b27a2f8053b49b51c24bac3
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-06-11 19:47:46 -0400

    Added some style

--- hello-world/react-15/index.js
+++ hello-world/react-15/index.js
@@ -1,9 +1,14 @@
 const { createElement } = React
 const { render } = ReactDOM
 
+const style = {
+    backgroundColor: 'purple',
+    color: 'white'
+}
+
 const title = createElement(
     'h1',
-    {id: 'title', className: 'header'},
+    {id: 'title', className: 'header', style},
     'Hello World'
 )
 
