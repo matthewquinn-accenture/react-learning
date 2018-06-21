96bc34a0a4647ec2273d1316c6d283a2c9a88cd2
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-06-20 21:36:43 -0400

    Added CSS and SCSS to webpack build

--- hello-world/react-15/src/lib.js
+++ hello-world/react-15/src/lib.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import text from './titles.json'
+import './stylesheets/farewell.scss'
+import './stylesheets/greeting.css'
 
 export const greeting = (
     <h1 id="title"
-        className="header"
-        style={{backgroundColor: 'orange', color: 'purple'}}> 
+        className="greeting">
         {text.hello}
     </h1>
 )
 
 export const farewell = (
     <h1 id="title"
-        className="header"
-        style={{backgroundColor: 'purple', color: 'orange'}}>
+        className="farewell">
         {text.goodbye}
     </h1>
 )
