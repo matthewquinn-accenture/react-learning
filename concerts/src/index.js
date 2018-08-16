10fef29ff4a7b50b6a4026c7043770db7c5101ad
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-08-15 20:44:06 -0400

    Setup initial routing

--- concerts/src/index.js
+++ concerts/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App.js'
+import { Whoops404 } from './components/Whoops404.js'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React /** Errors will show if 'React" isn't defined in window */
 
 render(
-    <App />,
+    <Router history={hashHistory}>
+        <Route path="/" component={App} />
+        <Route path="*" component={Whoops404} />
+    </Router>,
 		document.getElementById('react-container')
 )
